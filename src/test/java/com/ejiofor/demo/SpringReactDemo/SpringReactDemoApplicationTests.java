37fe14a3ffb77a59267c18cac60c5b393953d5ab
Christopher Ejiofor <cejiofor@gmail.com>
2020-01-11 16:15:28 -0600

    First commit

---
+++ src/test/java/com/ejiofor/demo/SpringReactDemo/SpringReactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejiofor.demo.SpringReactDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
